74bf2dac812714146dd8dbfd52b9fce1fd436fe3
odinokun <odinokun@gmail.com>
2018-06-23 11:12:31 +0300

    did header

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,8 @@ $(function() {
   // begin Slick slider
   // require('./modules/slick')();
 
-  // begin Popup
-  // require('./modules/popup')();
-
-  // begin Sticky header
-  // require('./modules/stickyHeader')();
-
   // begin Animate.css
   // require('./modules/waypoints')();
   // require('./modules/animateCss')();
-
-  // begin Fancybox3
-  // require('./modules/fancybox')();
 
 });
